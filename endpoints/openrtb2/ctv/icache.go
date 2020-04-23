a1625f8a566c783b5b973f537b82fbb8da5f7c30
Viral Vala <viral.vala@pubmatic.com>
2020-04-23 14:29:54 +0530

    Third Commit

--- endpoints/openrtb2/ctv/icache.go
+++
@@ -1,7 +0,0 @@
-package ctv
-
-//ICache Interface to store cached  combinations
-type ICache interface {
-	Set(xImp, xIndex, yImp, yIndex int, value bool)
-	Get(xImp, xIndex, yImp, yIndex int) (bool, bool)
-}
